58e4105d7f125ad4fa5102e4aff75dfdd3dcef74
Kirill <pajehyk@gmail.com>
2021-04-02 21:29:22 +0300

    пустая база данных

--- src/main/java/com/example/demo/Employee.java
+++ src/main/java/com/example/demo/Employee.java
@@ -17,7 +17,7 @@ class Employee {
     private String lastName;
     private String role;
 
-    Employee() {}
+    public Employee() {}
 
     Employee(String firstName, String lastName, String role) {
         this.firstName = firstName;
